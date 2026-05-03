f816e13b7c610abe488523cd7f5b79bf3e75e031
Tiffea <golovatenkoyan@gmail.com>
2026-05-03 18:14:23 +0300

    Add Terraform infrastructure as code

---
+++ terraform/main.tf
@@ -0,0 +1,87 @@
+#описываем что нужно чтобы начать работу(провайдер,версию и регион)
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1"
+}
+
+resource "aws_security_group" "devops_sg" {
+  name        = "devops-project-sg"
+  description = "Security group for Devops project"
+
+  #ssh?
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  #http?
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  #flask
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # Grafana
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  #prometheus
+  ingress {
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # all the traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "devops_server" {
+  ami                    = "ami-080254318c2d8932f"
+  instance_type          = "t3.micro"
+  key_name               = "devops-key"
+  vpc_security_group_ids = [aws_security_group.devops_sg.id]
+
+  tags = {
+    Name = "devops-project-server"
+  }
+}
+
+resource "aws_eip" "devops_eip" {
+    instance = aws_instance.devops_server.id
+
+    tags = {
+        Name = "devops-project-eip"
+    }
+}
+
+output "new_server_ip" {
+    value = aws_eip.devops_eip.public_ip
+}
