69d26e8e5ad61161c88d9a471e8bbb9e4a252c3e
Tiffea <golovatenkoyan@gmail.com>
2026-08-12 15:37:35 +0300

    Iac: add SSM-connection role for future db migration

--- infra/terraform/policies.tf
+++ infra/terraform/policies.tf
@@ -1,11 +1,13 @@
 
 
-
+########################################################################################
+#--------------------- SSH open protocol for remote instance control ------------------#
+#--------------------------------------------------------------------------------------#
 
 
 resource "aws_iam_role" "github_worker" {
   name = "github_worker"
-  description = "Role includes only 2 permissions: open port 22 and then close it"
+  description = "Role includes 2 permissions: open port 22 and then close it"
   assume_role_policy = jsonencode(
     {
     Version = "2012-10-17",
@@ -55,9 +57,13 @@ resource "aws_iam_role_policy_attachment" "github_worker" {
     policy_arn = aws_iam_policy.github_worker.arn
 }
 
+########################################################################################
+#--------------------- Assume role for EC2 DB inctance --------------------------------#
+#--------------------------------------------------------------------------------------#
+
 resource "aws_iam_role" "Role-for-EC2" {
     name = "Role-for-EC2"
-    description = "gives EC2 instance some autonomy" #description can be changed on a place
+    description = "assume role on instances" #description can be changed on a place
     assume_role_policy = jsonencode(
         {
             "Version": "2012-10-17",
@@ -83,4 +89,44 @@ resource "aws_iam_role_policy_attachment" "SSM_attachment_db" {
 resource "aws_iam_instance_profile" "DB_instance_profile" {
     name = "Role-for-EC2"
     role = aws_iam_role.Role-for-EC2.name
+}
+
+########################################################################################
+#--------------------- SMM CONNECTION POLICY -- ---------------------------------------#
+#--------------------------------------------------------------------------------------#
+
+resource "aws_iam_policy" "SSMconnection_policy" {
+    name = "SSMconnection_policy"
+    description = "give rights to open ssm session "
+    policy = jsonencode(
+        {
+            Version = "2012-10-17",
+            Statement = [
+                #allow connection between instance and SSM bot
+                {
+                    Effect = "Allow",
+                    Action = "ssm:StartSession",
+                    Resource = [
+                        "arn:aws:ec2:eu-north-1:515310962108:instance/i-01fc1da37e4c1bcb3",
+                        "arn:aws:ssm:*:*:document/AWS-StartSSHSession"
+                        #amazon-ssm-agent as a daemon process that listens to aws
+                    ]
+                },
+                #open channel to transmit data
+                {
+                    Effect = "Allow",
+                    Action = "ssmmessages:OpenDataChannel",
+                    Resource = "arn:aws:ssm:*:*:session/$${aws:userid}-*" 
+                    #user id is used for preventing 3rd party interuptions
+                    
+                }
+            ]
+        }
+    )
+}
+
+#linking
+resource "aws_iam_role_policy_attachment" "SSM_policy_attachment_db" {
+    role = aws_iam_role.github_worker.name
+    policy_arn = aws_iam_policy.SSMconnection_policy.arn
 }
