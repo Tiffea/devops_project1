4bbed3e6c071b08c70b5c8c0e8c4dd265e6d04f1
Tiffea <golovatenkoyan@gmail.com>
2026-08-14 13:41:52 +0300

    delete port 5000 form sec group; add "remove orphans" to ansible app  main.yml

--- infra/terraform/sec_groups.tf
+++ infra/terraform/sec_groups.tf
@@ -26,13 +26,6 @@ resource "aws_security_group" "devops1_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  #flask app
-  ingress {
-    from_port   = 5000
-    to_port     = 5000
-    protocol    = "tcp"
-    cidr_blocks = [local.my_ip]
-  }
   # Grafana
   ingress {
     from_port   = 3000
